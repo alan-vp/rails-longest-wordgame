74929bcdc2cb0ed4d33272eda78eeb46d0c05012
alan vergara <alan_vergara_p@hotmail.com>
2022-10-01 17:50:40 -0500

    score method and score view implemented, app works as requested

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,21 +1,30 @@
+require "open-uri"
 class GamesController < ApplicationController
   def new
     array_of_letters = Array('A'..'Z')
     @letters = Array.new(9) { array_of_letters.sample }.join(' ')
-    @word = params[:attemp]
   end
 
   def score
-    @word = params[:attemp] # word es un string que no sabemos si viene en mayúsculas o minúsculas.
-    # queremos saber si word está en la grid (método privado "word" in grid)
-    # si no lo esta (false) devolvemos mensaje que "la palabra no esta en la grid"
-    # si lo esta: (true)
-    # queremos saber que la word sea valida en ingles. (metodo privado valid?)
-    # si lo está (true damos puntaje)
-    # si no lo está devolvemos mensaje "que no es una palabra valida en ingles"
+    @word = (params[:attemp] || "").upcase # word es un string que no sabemos si viene en mayúsculas o minúsculas.
+    @letters = params[:letters]
+    @included = included_in_grid?(@word, @letters)
+    @english_word = english_word?(@word)
   end
 
-  def word_in_grid?
+  def included_in_grid?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+    # toma la palabra y la parte por letras separadas por coma en un array (chars).
+    # .all? itera letra por letra, y le pasa la condición.
+    # cuenta cuantas letras hay en word y compara que sea igual o menor
+    # que la cuenta de cuantas letras hay en grid.
 
+  end
+
+  def english_word?(word)
+    url = "https://wagon-dictionary.herokuapp.com/#{word}"
+    response = URI.open(url).read
+    parsed_response = JSON.parse(response)
+    parsed_response["found"]
   end
 end
